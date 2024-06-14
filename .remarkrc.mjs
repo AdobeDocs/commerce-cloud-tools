da2ae33f28f371fa125b8324cb06efb533ef2b8a
Dima Shevtsov <shevtsov@adobe.com>
2024-06-14 15:08:46 -0500

    Ignore valid link that fails the link check

--- .remarkrc.mjs
+++ .remarkrc.mjs
@@ -25,7 +25,8 @@ const remarkConfig = {
 			remarkLintNoDeadUrls,
 			{
 				skipUrlPatterns: [
-					"https://www.php.net"
+					"https://www.php.net",
+					"https://business.adobe.com/products/magento/magento-commerce.html"
 				]
 			}
 		]
